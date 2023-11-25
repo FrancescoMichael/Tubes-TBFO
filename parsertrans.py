df78e2905f71e545729b6449bee42ff95312469a
FrancescoMichael <francescokusuma15@gmail.com>
2023-11-25 18:52:57 +0700

    add tc tran

---
+++ parsertrans.py
@@ -0,0 +1,37 @@
+result = []
+
+source = 'trans.txt'
+f = open(source, 'r')
+s = f.readline()
+
+while s != '.':
+    n = len(s)
+    res = ""
+    i = 1
+    while(i < n):
+        if(s[i] == ','):
+            res += ' '
+            i+=1
+        elif(s[i] == ')'):
+            if(s[i+1] == '}'):
+                i = n
+            else: 
+                i+=5
+                res += ' '
+        else:
+            res+=s[i]
+        i+=1 
+    result.append(res)
+    s = f.readline()
+    
+f.close()
+
+dest = 'res.txt'
+
+f = open(dest, 'w')
+i = 0
+while i < len(result) - 1:
+    f.write(result[i] + '\n')
+    i+=1
+    
+f.close()
